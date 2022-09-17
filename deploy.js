6270c798816359987a1b00139d745cef4e56f4b5
Newbie814 <matthew.cyl.training@hotmail.com>
2022-09-16 23:32:19 -0400

    added deployment scripts

--- deploy.js
+++ deploy.js
@@ -1,3 +1,14 @@
-function hi() {
-  console.log('hi')
-}
+async function main() {}
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
+
+// setup movie night
+
+// cook popcorn
+// Pour drinks
+// Start Movie
+//
