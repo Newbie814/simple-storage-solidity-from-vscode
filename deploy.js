236e962790ed6064cde086ae5c07059a566a14b1
Newbie814 <matthew.cyl.training@hotmail.com>
2022-09-17 19:07:44 -0400

    built ganache server, encrypted env variables, updated prettier config

--- deploy.js
+++ deploy.js
@@ -3,24 +3,22 @@ const ethers = require('ethers');
 const fs = require('fs-extra');
 require('dotenv').config();
 
-// const ganache = require('ganache');
-
-// const provider = new ethers.providers.Web3Provider(ganache.provider());
-
 async function main() {
   //http://127.0.0.1:7545
   const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
-  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+  // const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+  const encryptedJson = fs.readFileSync('./.encryptedKey.json', 'utf8');
+  let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+    encryptedJson,
+    process.env.PRIVATE_KEY_PASSWORD
+  );
+  wallet = await wallet.connect(provider);
+
   const abi = fs.readFileSync('./SimpleStorage_sol_SimpleStorage.abi', 'utf8');
   const binary = fs.readFileSync(
     './SimpleStorage_sol_SimpleStorage.bin',
     'utf8'
   );
-
-  // const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
-  // console.log('Deploying contract...');
-
-  // const contract = await contractFactory.deploy();
 
   const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
   console.log('Deploying, please wait...');
@@ -30,7 +28,6 @@ async function main() {
   console.log(contract.deployTransaction);
   console.log('transaction receipt:');
   console.log(transactionReceipt);
-  console.log(contract);
 }
 
 main()
